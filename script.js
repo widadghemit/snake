773f6c41d11d559ebd2eeaabb32f2d130e0b99e4
widadghemit <ghemit.widad@gmail.com>
2025-06-17 17:47:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const tileCount = canvas.width / gridSize;
 let snake, direction, food, gameOver;
 let speed = 300; // vitesse initiale en ms (lente)
 let gameInterval;
-let lastLength = 1; // longueur du serpent à la dernière frame
+let lastLength = 1;
 
 function initGame() {
   snake = [{ x: 10, y: 10 }];
@@ -27,11 +27,12 @@ function gameLoop() {
   draw();
 
   if (!gameOver) {
-    // Accélération douce à chaque nourriture mangée
     if (snake.length > lastLength) {
       lastLength = snake.length;
-      if (speed > 50) {
-        speed -= 5; // accélère un peu
+
+      // Accélération tous les 3 blocs mangés
+      if ((snake.length - 1) % 3 === 0 && speed > 100) {
+        speed -= 2; // accélération plus douce
         clearInterval(gameInterval);
         gameInterval = setInterval(gameLoop, speed);
       }
@@ -48,32 +49,26 @@ function draw() {
     return;
   }
 
-  // Fond
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Nourriture
   ctx.fillStyle = "red";
   ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
 
-  // Serpent
   ctx.fillStyle = "lime";
   for (let part of snake) {
     ctx.fillRect(part.x * gridSize, part.y * gridSize, gridSize, gridSize);
   }
 
-  // Déplacement
   const head = { x: snake[0].x + direction.x, y: snake[0].y + direction.y };
   snake.unshift(head);
 
-  // Collision avec la nourriture
   if (head.x === food.x && head.y === food.y) {
     placeFood();
   } else {
     snake.pop();
   }
 
-  // Collision mur ou corps
   if (
     head.x < 0 || head.x >= tileCount ||
     head.y < 0 || head.y >= tileCount ||
